994318b7c1229d96d4ec24a2bbf1093c17ce87b3
mrweckel <mrweckel@gmail.com>
2015-02-14 21:43:41 -0500

    set up mongodb

--- config/models.js
+++ config/models.js
@@ -11,13 +11,7 @@
 
 module.exports.models = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Your app's default connection. i.e. the name of one of your app's        *
-  * connections (see `config/connections.js`)                                *
-  *                                                                          *
-  ***************************************************************************/
-  // connection: 'localDiskDb',
+  connection: 'myMongodbServer'
 
   /***************************************************************************
   *                                                                          *
