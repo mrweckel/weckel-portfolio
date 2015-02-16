170a0088058c4f374d5dc65817f2015a41b927c2
mrweckel <mrweckel@gmail.com>
2015-02-15 20:40:53 -0500

    add content controller and array of works -- angular

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,156 @@
+(function(){
+  'use strict'
+
+  var app = angular.module('port', []);
+
+  app.controller('ContentController',function(){
+      this.projects = works;
+  });
+
+  var works = [
+    {
+      title: 'Ferrari Scuderia',
+      category: 'film',
+      video_url: 'https://www.youtube.com/watch?v=31in9i03OOs',
+      url: false,
+      github: false,
+      content: '',
+      images: [
+        {
+          full: '',
+          thumb: '/images/content_thumbs/ferrari-thm.jpg'
+        }
+      ]
+    },
+    {
+      title: 'The Food Finder',
+      category: 'development',
+      video_url: false,
+      url: false,
+      github: 'https://github.com/mrweckel/the-food-finder',
+      content: '',
+      images: [
+        {
+          full: '',
+          thumb: '/images/content_thumbs/food-finder-thm.jpg'
+        }
+      ]
+    },
+    {
+      title: 'Intel Make It Wearable Series',
+      category: 'film',
+      video_url: 'https://www.youtube.com/watch?v=O0iPNr-142Q',
+      url: false,
+      github: false,
+      content: '',
+      images: [
+        {
+          full: '',
+          thumb: '/images/content_thumbs/wearable-thm.jpg'
+        }
+      ]
+    },
+    {
+      title: 'Mastermind: Sly Stallone Edition',
+      category: 'development',
+      video_url: false,
+      url: 'http://mastermind-stallone-edition.herokuapp.com/',
+      github: 'https://github.com/mrweckel/Mastermind-Stallone-Edition',
+      content: '',
+      images: [
+        {
+          full: '',
+          thumb: '/images/content_thumbs/stallone-thm.jpg'
+        }
+      ]
+    },
+    {
+      title: 'Dreams',
+      category: 'development',
+      video_url: false,
+      url: 'http://memoryfinder.heroku.app',
+      github: 'https://github.com/mrweckel/dreams',
+      content: '',
+      images: [
+        {
+          full: '',
+          thumb: '/images/content_thumbs/dreams-thm.jpg'
+        }
+      ]
+    },
+    {
+      title: 'Static: Music Video',
+      category: 'film',
+      video_url: 'https://www.youtube.com/watch?v=oEgbn_OZJ9Y',
+      url: false,
+      github: false,
+      content: '',
+      images: [
+        {
+          full: '',
+          thumb: '/images/content_thumbs/static-thm.jpg'
+        }
+      ]
+    },
+    {
+      title: 'Youtube Music Awards',
+      category: 'film',
+      video_url: 'https://www.youtube.com/watch?v=ZXlSgJIE8Io',
+      url: false,
+      github: 'https://github.com/mrweckel/dreams',
+      content: '',
+      images: [
+        {
+          full: '',
+          thumb: '/images/content_thumbs/youtube-thm.jpg'
+        }
+      ]
+    },
+    {
+      title: 'PitchStart',
+      category: 'development',
+      video_url: false,
+      url: false,
+      github: 'https://github.com/mrweckel/PitchStart',
+      content: '',
+      images: [
+        {
+          full: '',
+          thumb: '/images/content_thumbs/pitchstarter-thm.jpg'
+        }
+      ]
+    },
+    {
+      title: 'Crossing Dumbo',
+      category: 'film',
+      video_url: 'https://vimeo.com/35782011',
+      url: false,
+      github: false,
+      content: '',
+      images: [
+        {
+          full: '',
+          thumb: '/images/content_thumbs/crossing-dumbo-thm.jpg'
+        }
+      ]
+    }
+  ]
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* particlesJS('dom-id', params);
 /* @dom-id : set the html tag id [string, optional, default value : particles-js]
 /* @params: set the params [object, optional, default values : check particles.js] */
