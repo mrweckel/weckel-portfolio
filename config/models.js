8c4c89f425160bb57a92c3b66378e4862b023c74
mrweckel <mrweckel@gmail.com>
2015-02-17 18:28:38 -0500

    add missing comma

--- config/models.js
+++ config/models.js
@@ -11,7 +11,7 @@
 
 module.exports.models = {
 
-  connection: 'myMongodbServer'
+  connection: 'myMongodbServer',
 
   /***************************************************************************
   *                                                                          *
