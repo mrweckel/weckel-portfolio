086d89e35595d948219ff9455e5b45e595b9128e
mrweckel <mrweckel@gmail.com>
2015-02-17 21:40:48 -0500

    add Sass wherever less was in project

--- tasks/register/syncAssets.js
+++ tasks/register/syncAssets.js
@@ -1,8 +1,8 @@
 module.exports = function (grunt) {
 	grunt.registerTask('syncAssets', [
 		'jst:dev',
-		'less:dev',
-    // 'sass:dev',
+		// 'less:dev',
+    'sass:dev',
 		'sync:dev',
 		'coffee:dev'
 	]);
